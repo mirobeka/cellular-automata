d1cb4520b34b13d7906f2eb67ea5bc3cf51fb686
mirobeka <mirobeka@gmail.com>
2013-01-21 15:16:04 +0100

    initial version of loading lattice form file #31
    	modified:   cellular_automata/lattices/base.py

--- cellular_automata/lattices/base.py
+++ cellular_automata/lattices/base.py
@@ -3,3 +3,15 @@ class Lattice:
     # go over all cells and perform next step
     raise NotImplementedError("method nextStep of Lattice not implemented")
 
+  @classmethod
+  def loadFromFile(cls, filename):
+    '''
+    load configuration of lattice, create cells, set cells states
+    neighbours and other stuff
+
+    this should be lattice specific, so this is just abstract class method
+    '''
+    raise NotImplementedError("Method loadFromFile of Lattice is not implemented")
+    
+
+  
