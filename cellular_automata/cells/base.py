001f2b0ec0ffb0912698b7b4140f6f4ad4de28d0
mirobeka <mirobeka@gmail.com>
2013-01-22 15:44:19 +0100

    Adds position property into cell #25
    	modified:   cellular_automata/cells/base.py

--- cellular_automata/cells/base.py
+++ cellular_automata/cells/base.py
@@ -1,14 +1,43 @@
-class Cell:
+class Cell(object):
   def __init__(self, rule):
     self.rule = rule
     self.neighs = None
     self.age = 0
+    self._position = (0,0)
     self.createState()
 
   def createState(self):
     self.state = {}
     self.state["current"] = None
     self.state["next"] = None
+
+  # few property getters and setters
+  @property
+  def position(self):
+    return self._position
+
+  @position.setter
+  def position(self, newPos):
+    if type(newPos) == tuple and len(newPos) == 2:
+      self._position = newPos
+    else:
+      raise Exception("wrong position argument: {}".format(newPos))
+
+  @property
+  def x(self):
+    return self._position[0]
+
+  @x.setter
+  def x(self, x):
+    self._position = (x, self.y)
+
+  @property
+  def y(self):
+    return self._position[1]
+
+  @y.setter
+  def y(self, y):
+    self._position = (self.x, y)
 
   def computeNextState(self):
     self.state["next"] = self.rule.getNextState(self, self.neighs)
