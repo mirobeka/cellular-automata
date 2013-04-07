78267909c415444cf73da39460bc08345f7fd991
mirobeka <mirobeka@gmail.com>
2013-04-08 00:51:43 +0200

    Initial version of irregular cell
    	new file:   cellular_automata/cells/irregular.py

---
+++ cellular_automata/cells/irregular.py
@@ -0,0 +1,11 @@
+from cellular_automata.cells.base import Cell
+
+
+class IrregularCell(Cell):
+    """ This cell is for irregular tessellation of lattice such as voronoi
+    tessellation.
+
+    """
+
+    def add_neighbours(self):
+        pass
