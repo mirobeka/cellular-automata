f734d702f901c60eb8140250e0f44b2f0e70334f
mirobeka <mirobeka@gmail.com>
2013-04-08 00:08:15 +0200

    Adds support for mapping color into cell state variable
    	modified:   evosim/simulation.py

--- evosim/simulation.py
+++ evosim/simulation.py
@@ -31,41 +31,86 @@ class Simulation(object):
         LatticeWidget.set_cell_state = color_to_state
 
     def smart_state_mapping(self):
-        # get sample state
+        """ dynamically create method for mapping state of cell into color
+        space and back from color into cell state. Method is created based on
+         type of state variable and it's name.
+        :return: tuple of methods state -> rgb, rgb -> state
+        """
         sample_state = self.get_state_instance()
 
         for state in SUPPORTED_STATES:
             if hasattr(sample_state, state):
                 state_to_color = self.create_color_mapping(getattr(
                     sample_state, state), state)
-                color_to_state = self.create_state_mapping()
+                color_to_state = self.create_state_mapping(getattr(
+                    sample_state, state), state)
                 break
         return state_to_color, color_to_state
 
-    def create_state_mapping(self):
-        pass
+    def create_state_mapping(self, value, state_type):
+        """This method creates method that is used to override not
+        implemented LatticeWidget method for mapping rgb color into cell state
+
+        :param value: sample value of state variable. Just to know what type it is
+        :param state_type: name of attribute of this state variable
+        :return: method map_state_to_rgb
+        """
+        length = 1
+        try:
+            length = len(value)
+        except TypeError:
+            print("value {} is not tuple or list".format(value))
+            print("mapping to grayscale")
+        else:
+            length = 1
+
+        # this is super ugly, find better way to do this
+        if length == 1:
+            if type(value) is float:
+                def map_rgb_to_state(self, rgb, state):
+                    setattr(state, state_type, rgb[0]/255.0)
+                return map_rgb_to_state
+            else:
+                def map_rgb_to_state(self, rgb, state):
+                    setattr(state, state_type, rgb[0])
+        else:
+            if type(value[0]) is float:
+                def map_rgb_to_state(self, rgb, state):
+                    setattr(state, state_type, tuple([map(lambda x: x/255.0),
+                                                      rgb]))
+                return map_rgb_to_state
+            else:
+                def map_rgb_to_state(self, rgb, state):
+                    setattr(state, state_type, rgb)
+                return map_rgb_to_state
 
     def create_color_mapping(self, value, state_type):
+        """This method creates method that is used to override not
+        implemented LatticeWidget method for mapping state of cell into color
+         space
+
+        :param value: sample value of state variable. Just to know what type it is
+        :param state_type: name of attribute of this state variable
+        :return: method map_state_to_rgb
+        """
         length = 1
         try:
             length = len(value)
         except TypeError as e:
-            print("value {} is probably float or int".format(value))
+            print("value {} is not tuple or list".format(value))
             print("mapping to grayscale")
         else:
             length = 1
 
         if length == 1:
-            return self.create_mapping("#{val:02X}{val:02X}{val:02X}",
-                                       state_type)
+            def map_state_to_rgb(self, state):
+                return "#{val:02X}{val:02X}{val:02X}".format(val=getattr(
+                    state, state_type))
+            return map_state_to_rgb
         else:
-            return self.create_mapping("#{:02X}{:02X}{:02X}", state_type)
-
-    @staticmethod
-    def create_mapping(mapping_string, attr_name):
-        def map_state_to_rgb(self, state):
-            return mapping_string.format(val=getattr(state, attr_name))
-        return map_state_to_rgb
+            def map_state_to_rgb(self, state):
+                return "#{:02X}{:02X}{:02X}".format(getattr(state, state_type))
+            return map_state_to_rgb
 
     def get_state_instance(self):
         conf = get_conf(self.conf_file)
