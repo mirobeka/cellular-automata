431e768ce70c19e4b61c387cfa63e083720494c3
mirobeka <mirobeka@gmail.com>
2013-04-17 01:26:55 +0200

    New way of creating Objectives
    	new file:   objectives/Base.py
    	renamed:    objectives/objectives.py -> objectives/shapes.py

---
+++ objectives/Base.py
@@ -0,0 +1,35 @@
+class Objective(object):
+    """This is just abstract class to be extended. Extend this class for your
+    objective that should be optimized.
+    """
+
+    def error_function(self, pattern, lattice):
+        """ error funtion compare given latice with desired pattern and returns
+        scalar value describing difference between those 2 lattices
+
+        This method should be implemented to reflect objective that should be
+        optimized.
+
+        For example, if I want to optimized structure and topology of cellular
+        automata(CA), in this function should be compared resulting lattice
+        and  defined pattern and thid comparison reflected by scalar value.
+
+        In other case, I we want to optimise structure and also states of CA
+         se should compare states and structure and then sum this error and
+         return scalar value
+        """
+        raise NotImplementedError("error function not implemented")
+
+    def objective_function(self, vector):
+        """ Objective funtion is main function that is minimized by cma-es
+        algorithm. In our case, we are optimizing weights of neural network,
+        but we can optimize anything we want.
+
+        On the input of objective_function is optimized vector of values. On
+        the output of objective_function must be scalar value that is minimized.
+
+
+        This method should be implemented in a way that suits your
+        optimalization needs.
+        """
+        raise NotImplementedError("method objective_function not implemented")
--- objectives/shapes.py
+++ objectives/shapes.py
@@ -1,55 +1,28 @@
+from objectives.base import Objective
 from cellular_automata.creator import create_automaton
 from cellular_automata.creator import load_automaton
 
 
-class Objective(object):
-    """This is just abstract class to be extended. Extend this class for your
-    objective that should be optimized.
-    """
-
-    def error_function(self, pattern, lattice):
-        """ error funtion compare given latice with desired pattern and returns
-        scalar value describing difference between those 2 lattices
-
-        This method should be implemented to reflect objective that should be
-        optimized.
-
-        For example, if I want to optimized structure and topology of cellular
-        automata(CA), in this function should be compared resulting lattice
-        and  defined pattern and thid comparison reflected by scalar value.
-
-        In other case, I we want to optimise structure and also states of CA
-         se should compare states and structure and then sum this error and
-         return scalar value
-        """
-        raise NotImplementedError("error function not implemented")
-
-    def objective_function(self, vector):
-        """ Objective funtion is main function that is minimized by cma-es
-        algorithm. In our case, we are optimizing weights of neural network,
-        but we can optimize anything we want.
-
-        On the input of objective_function is optimized vector of values. On
-        the output of objective_function must be scalar value that is minimized.
-
-
-        This method should be implemented in a way that suits your
-        optimalization needs.
-        """
-        raise NotImplementedError("method objective_function not implemented")
-
-
 class TwoBandObjective(Objective):
     def __init__(self, lattice_file):
-        Objective.__init__(self)
-        self.lattice_file = lattice_file
-        self.initialize_experiment_parameters()
+        self.initialize_experiment_parameters(lattice_file)
 
-    def initialize_experiment_parameters(self):
-        self.desired_lattice = load_automaton(self.lattice_file)
+    @classmethod
+    def create_new_objective(cls, lattice_file):
+        objective = cls(lattice_file)
+        return objective
+
+    def initialize_experiment_parameters(self, lattice_file):
+        # load desired automaton from serialized file
+        self.desired_lattice = load_automaton(lattice_file)
+
+        # load all important properties
         self.dimensions = (self.desired_lattice.width,
                            self.desired_lattice.height)
         self.resolution = self.desired_lattice.resolution
+
+        # create energy stop criterion. This way is good for now,
+        # later we could be more flexible about choosing stop criterion
         self.stop_criterion = EnergyStopCriterion()
         self.max_difference = self.get_max_difference(self.desired_lattice)
 
