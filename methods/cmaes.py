7788cdf95c956df5bef9dac0a182c97bcfff6d0c
mirobeka <mirobeka@gmail.com>
2013-04-25 00:52:17 +0200

    Unsing pybrain cmaes instead of separate library
    	modified:   evosim/evolution.py
    	modified:   experiments/basic_simulation.cfg
    	new file:   methods/cmaes.py

---
+++ methods/cmaes.py
@@ -0,0 +1,7 @@
+from numpy import array
+from pybrain.optimization import CMAES
+
+def learn(obj_fun, init_values):
+    l = CMAES(obj_fun, init_values, minimize=True)
+    res = l.learn()
+    return res[0]
