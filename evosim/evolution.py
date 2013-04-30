8228109c661c6475328b9ff8c4509961f628de7a
mirobeka <mirobeka@gmail.com>
2013-04-30 02:36:15 +0200

    Automatically handle arbitrary number of weights
    	modified:   evosim/evolution.py

--- evosim/evolution.py
+++ evosim/evolution.py
@@ -50,7 +50,15 @@ class Evolution(object):
         self.result = self.strategy.learn(self.objective.objective_function, initial_values)
 
     def get_initial_values(self):
-        values = eval(self.conf["evolution"]["initial_weights"])
+        rule_class = self.conf["simulation"]["rule"]
+        if self.conf["simulation"].has_key("chemical_vector_length"):
+            chems_count = int(self.conf["simulation"]["chemical_vector_length"])
+            inter_count = int(self.conf["simulation"]["internal_vector_length"])
+            sample_rule_instance = rule_class(chems_count, inter_count)
+        else:
+            sample_rule_instance = rule_class()
+        number_of_values = sample_rule_instance.total_number_of_weights()
+        values = np.random.rand(number_of_values)
         return np.array(values)
 
     def save_results(self, file_name="data/result"):
