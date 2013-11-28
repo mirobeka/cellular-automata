f5e82cf790153518be1f446dc2973a55d121898f
mirobeka <mirobeka@gmail.com>
2013-11-28 11:28:37 +0100

    Rename web app to WICA

--- wica/growing_ca.py
+++ wica/growing_ca.py
@@ -9,12 +9,12 @@ Cake(app)
 
 @app.route("/")
 def intro():
-  return render_template("intro.html")
+  return render_template("intro.jinja")
 
 @app.route("/config/")
 def config():
   config_options = get_options()
-  return render_template("config.html")
+  return render_template("config.jinja", config_option=config_options)
 
 if __name__ == "__main__":
   app.run()
