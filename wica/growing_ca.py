71683d3f280e63e94f93b17981363ba8b47c1609
mirobeka <mirobeka@gmail.com>
2013-11-29 02:57:09 +0100

    Refactor controller, routing + add new controllers
    modified:   wica/growing_ca.py

--- wica/growing_ca.py
+++ wica/growing_ca.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask_cake import Cake
 from config_options import get_options
 
@@ -8,13 +10,29 @@ app.debug = True
 Cake(app)
 
 @app.route("/")
-def intro():
-  return render_template("intro.jinja")
+def index():
+  return redirect(url_for("dashboard"))
 
-@app.route("/config/")
-def config():
+@app.route("/dashboard/")
+def dashboard():
+  return render_template("dashboard.jinja")
+
+@app.route("/configurations/")
+def configurations():
   config_options = get_options()
-  return render_template("config.jinja", config_option=config_options)
+  return render_template("configurations.jinja", config_option=config_options)
+
+@app.route("/replays/")
+def replays():
+  return render_template("replays.jinja")
+
+@app.route("/about/")
+def about():
+  return render_template("about.jinja")
+
+@app.route("/documentation/")
+def documentation():
+  return render_template("documentation.jinja")
 
 if __name__ == "__main__":
   app.run()
