a0f4f23accaefdc3ec861249e5e1835ea4226821
mirobeka <mirobeka@gmail.com>
2014-02-04 00:47:20 +0100

    Improve listing of created projects #41
    modified:   wica/growing_ca.py
    modified:   wica/templates/project.jinja
    modified:   wica/templates/projects.jinja
    modified:   wica/templates/view_helpers.jinja

--- wica/growing_ca.py
+++ wica/growing_ca.py
@@ -6,11 +6,12 @@ from flask import abort
 from flask import url_for
 from flask_cake import Cake
 from config_options import get_options
+import logging
 import os
 import sys
 
-#add cellular automata into path
-sys.path.insert(0, "..")
+#add cellular automata into path. Must be run from root directory
+sys.path.insert(0, ".")
 
 from projects.project import Project
 
@@ -43,10 +44,28 @@ def get_project(project_name):
     return abort(404)
   return render_template("project.jinja", project=project)
 
+@app.route("/projects/<project_name>/", methods=["PUT"])
+def update_project_config(project_name):
+  project = Project.load_project(project_name)
+  if project is None:
+    return abort(404)
+
+  project.update_config(request.form)
+  project.save()
+  return url_for("get_project", project_name=project_name)
+
+@app.route("/projects/<project_name>/", methods=["DELETE"])
+def delete_project(project_name):
+  project = Project.load_project(project_name)
+  if project is None:
+    return abort(404)
+  project.delete()
+  return url_for("get_projects")
+
 @app.route("/projects/", methods=["GET"])
 def get_projects():
-  config_options = get_options()
-  return render_template("projects.jinja", config_option=config_options)
+  projects = Project.load_projects()
+  return render_template("projects.jinja", projects=projects)
 
 @app.route("/replays/")
 def replays():
@@ -60,5 +79,15 @@ def about():
 def documentation():
   return render_template("documentation.jinja")
 
+def set_logger(level="DEBUG", outfile=None):
+  """Configure logger"""
+  logging.basicConfig(
+      format="%(asctime)s - [%(name)s.%(levelname)s] %(message)s",
+      datefmt="%I:%M:%S %p",
+      filename=outfile,
+      filemode="w",
+      level=level.upper())
+
 if __name__ == "__main__":
+  set_logger()
   app.run()
