d7b5d1c4bd0baead5e1b7871880335f84ad55d41
mirobeka <mirobeka@gmail.com>
2014-02-25 11:00:16 +0100

    change tabstop to 4 spaces #16
    modified:   wica/growing_ca.py

--- wica/growing_ca.py
+++ wica/growing_ca.py
@@ -21,98 +21,98 @@ Cake(app)
 
 @app.route("/")
 def index():
-  return redirect(url_for("dashboard"))
+    return redirect(url_for("dashboard"))
 
 @app.route("/dashboard/")
 def dashboard():
-  return render_template("dashboard.jinja")
+    return render_template("dashboard.jinja")
 
 @app.route("/projects/", methods=["POST"])
 def create_project():
-  project_name = request.form["projectName"]
+    project_name = request.form["projectName"]
 
-  # TODO: check if project with this name already exists
+    # TODO: check if project with this name already exists
 
-  # creates project
-  project = Project.create_project(project_name)
-  return url_for("get_project", project_name=project_name)
+    # creates project
+    project = Project.create_project(project_name)
+    return url_for("get_project", project_name=project_name)
 
 @app.route("/projects/<project_name>/replay/<replay_name>/", methods=["GET"])
 def get_project_replay_data(project_name, replay_name):
-  log = logging.getLogger("PROJECT")
-  log.debug("project_name = {} replay_name = {}".format(project_name, replay_name))
+    log = logging.getLogger("PROJECT")
+    log.debug("project_name = {} replay_name = {}".format(project_name, replay_name))
 
-  project = Project.load_project(project_name)
-  if project is None:
-    return abort(404)
-  json_data = convert_to_json(project.replay(replay_name))
-  return json_data
+    project = Project.load_project(project_name)
+    if project is None:
+        return abort(404)
+    json_data = convert_to_json(project.replay(replay_name))
+    return json_data
 
 @app.route("/projects/<project_name>/", methods=["GET"])
 @app.route("/projects/<project_name>/<tab>/", methods=["GET"])
 def get_project(project_name, tab="settings"):
-  project = Project.load_project(project_name)
-  if project is None:
-    return abort(404)
-  if tab == "settings":
-    return render_template("project_settings.jinja", project=project)
-  elif tab == "replays":
-    return render_template("project_replays.jinja", project=project)
-  elif tab == "evolve":
-    return render_template("project_evolve.jinja", project=project)
+    project = Project.load_project(project_name)
+    if project is None:
+        return abort(404)
+    if tab == "settings":
+        return render_template("project_settings.jinja", project=project)
+    elif tab == "replays":
+        return render_template("project_replays.jinja", project=project)
+    elif tab == "evolve":
+        return render_template("project_evolve.jinja", project=project)
 
 
 @app.route("/projects/<project_name>/", methods=["PUT"])
 def update_project_config(project_name):
-  log = logging.getLogger("WICA")
-  log.debug("trying update project {}".format(project_name))
-  project = Project.load_project(project_name)
-  if project is None:
-    return abort(404)
+    log = logging.getLogger("WICA")
+    log.debug("trying update project {}".format(project_name))
+    project = Project.load_project(project_name)
+    if project is None:
+        return abort(404)
 
-  project.update_config(request.form)
-  project.save()
-  return url_for("get_project", project_name=project_name)
+    project.update_config(request.form)
+    project.save()
+    return url_for("get_project", project_name=project_name)
 
 @app.route("/projects/<project_name>/", methods=["DELETE"])
 def delete_project(project_name):
-  log = logging.getLogger("WICA")
-  log.debug("deleting project {}".format(project_name))
-  project = Project.load_project(project_name)
-  if project is None:
-    return abort(404)
-  project.delete()
-  return url_for("get_projects")
+    log = logging.getLogger("WICA")
+    log.debug("deleting project {}".format(project_name))
+    project = Project.load_project(project_name)
+    if project is None:
+        return abort(404)
+    project.delete()
+    return url_for("get_projects")
 
 @app.route("/projects/", methods=["GET"])
 def get_projects():
-  projects = Project.load_projects()
-  return render_template("projects.jinja", projects=projects)
+    projects = Project.load_projects()
+    return render_template("projects.jinja", projects=projects)
 
 @app.route("/replays/")
 def replays():
-  return render_template("replays.jinja")
+    return render_template("replays.jinja")
 
 @app.route("/about/")
 def about():
-  return render_template("about.jinja")
+    return render_template("about.jinja")
 
 @app.route("/documentation/")
 def documentation():
-  return render_template("documentation.jinja")
+    return render_template("documentation.jinja")
 
 def set_logger(level="DEBUG", outfile=None):
-  """Configure logger"""
-  logging.basicConfig(
-      format="%(asctime)s - [%(name)s.%(levelname)s] %(message)s",
-      datefmt="%I:%M:%S %p",
-      filename=outfile,
-      filemode="w",
-      level=level.upper())
+    """Configure logger"""
+    logging.basicConfig(
+        format="%(asctime)s - [%(name)s.%(levelname)s] %(message)s",
+        datefmt="%I:%M:%S %p",
+        filename=outfile,
+        filemode="w",
+        level=level.upper())
 
 def convert_to_json(data):
-  return json.dumps(data)
+    return json.dumps(data)
 
 if __name__ == "__main__":
-  set_logger()
-  app.run()
+    set_logger()
+    app.run()
