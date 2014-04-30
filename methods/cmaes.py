c90ea515ca27ecf2c50019305e8630ebf4769dd2
mirobeka <mirobeka@gmail.com>
2014-04-30 19:45:29 +0200

    Add callback to cmaes, change arguments
    modified:   methods/cmaes.py

--- methods/cmaes.py
+++ methods/cmaes.py
@@ -2,10 +2,16 @@ from pybrain.optimization import CMAES
 from numpy import array
 import logging
 
-def learn(obj_fun, inital_weights):
-    l = CMAES(obj_fun, inital_weights, minimize=True, verbose=True)
+log = logging.getLogger("CMAES")
+
+def learn(objective, inital_weights):
+    l = CMAES(objective.objective_function, inital_weights, minimize=True, verbose=True, )
+
+    l.maxLearningSteps = 2
+    l._callback = objective.generation_callback
+
+    log.debug("starting cmaes evolution")
     res = l.learn()
-    log = logging.getLogger("CMAES")
     log.debug("result is: {}".format(res))
     return res[0]
 
