bbb6d845b071c527d643250002e2fde2af80ede8
mirobeka <mirobeka@gmail.com>
2014-05-01 09:18:51 +0200

    Adds new FFN rule for moore neighbourhood
    modified:   cellular_automata/rules/neural_rule.py

--- cellular_automata/rules/neural_rule.py
+++ cellular_automata/rules/neural_rule.py
@@ -150,6 +150,96 @@ class HardCodedRule3(Rule):
     def set_border(self, border=None):
         self.border = border
 
+class FeedForwardNetworkMoore(Rule):
+    def __init__(self, internal_state_vector_length=1):
+        self.internal_state_vector_length = internal_state_vector_length
+        self.input_layer_length = 9 * self.internal_state_vector_length
+        self.hidden_layer_length = self.input_layer_length - 4
+        self.output_layer_length = self.internal_state_vector_length
+        self.randomize_weights()
+
+    def randomize_weights(self):
+        number_of_weights = self.total_number_of_weights()
+        weights = np.random.rand(number_of_weights)
+        self.set_weights(weights)
+
+    def total_number_of_weights(self):
+        """Return total number of weights that are used"""
+        return (self.input_layer_length + 1) * self.hidden_layer_length + (
+            self.hidden_layer_length + 1) * self.output_layer_length
+
+    def set_weights(self, new_weights):
+        """Weights we are evolving are passed to neural network by this function.
+        list of weight values are sorted like this.
+
+        input -> hidden layer weights = ( 5 * state vector + 1 ) * hidden layer length
+        hidden -> output layer weights = ( hidden l. length + 1 ) * ( state vector + color vector )
+        """
+
+        self.weights = new_weights
+        if type(new_weights) is list:
+            new_weights = np.array(new_weights)
+
+        first_slice = (self.input_layer_length + 1) * self.hidden_layer_length
+        self.theta1 = np.array(new_weights[:first_slice])
+        self.theta1.shape = (
+            self.hidden_layer_length, self.input_layer_length + 1)
+
+        second_slice = (self.hidden_layer_length + 1) * self.output_layer_length
+        self.theta2 = new_weights[first_slice:first_slice + second_slice]
+        self.theta2.shape = (
+            self.output_layer_length, self.hidden_layer_length + 1)
+
+    def get_weights(self):
+        return self.weights
+
+    def get_next_state(self, cell, neighbours):
+        # get input vector from neighbours
+        input_vector = self.get_input_vector(cell, neighbours)
+
+        # get hidden layer
+        a1 = np.insert(input_vector, 0, 1)
+        hidden_layer = np.tanh(np.dot(self.theta1, a1))
+
+        # get output layer
+        a2 = np.insert(hidden_layer, 0, 1)
+        out_vector = np.tanh(np.dot(self.theta2, a2))
+
+        # set new internal state of cell
+        new_internal_state = out_vector[0]
+        new_grayscale_color = 255*(new_internal_state+1)/2
+
+        # finally create new state
+        new_state = cell.state.create_state()
+        new_state.internal = new_internal_state
+        new_state.grayscale = max(min(int(new_grayscale_color), 255), 0)
+        return new_state
+
+    def get_input_vector(self, cell, neighbours):
+        """ Here we are dealing with just one neighbour per direction. It's Square
+        Lattice so there is just one neighbour per direction.
+        """
+        input_list = []
+        input_list.append(cell.state.internal)
+        for direction in ["north","northeast", "east", "southeast", "south", "southwest", "west", "northwest"]:
+            if len(neighbours[direction]) == 0:
+                border_value = self.get_border_value(cell.position, direction)
+                input_list.extend(border_value)
+            else:
+                input_list.append(
+                    iter(neighbours[direction]).next().state.internal)
+
+        if len(input_list) != self.input_layer_length:
+            print("{0} != {1}".format(len(input_list), self.input_layer_length))
+            raise Exception("invalid input layer length")
+        return np.array(input_list)
+
+    def get_border_value(self, coords, direction):
+        return self.border[coords][direction]
+
+    def set_border(self, border=None):
+        self.border = border
+
 class FeedForwardNetwork(Rule):
     def __init__(self, internal_state_vector_length=1):
         self.internal_state_vector_length = internal_state_vector_length
